0eea3520eb16be4ad91444f1fdb6b9b258353447
Kento <kento2811@gmail.com>
2025-06-23 18:10:34 +0700

    Initial commit - Dự án quản lý nhà hàng

---
+++ Login/Program.cs
@@ -0,0 +1,17 @@
+﻿def dang_nhap(username, password, remember_me):
+    if not username or not password:
+        return "Điền đầy đủ thông tin"
+
+    if not username.replace("_", "").isalnum():
+        return "Username không được chứa ký tự đặc biệt"
+
+    user = tim_user_trong_database(username)
+    if not user or user.password != password:
+        return "Username hoặc password không đúng"
+
+    if remember_me:
+        tao_session(ngay_het_han = 7)
+    else:
+        tao_session(ngay_het_han = "khi đóng trình duyệt")
+
+    return f"Chào {username}"
---
+++ us1.cs
@@ -0,0 +1,3 @@
+// Thêm kiểm tra nếu password null hoặc trống
+if (string.IsNullOrEmpty(password)) return false;
+//abcd
